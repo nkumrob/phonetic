310b37ce27747c895feaddd920564f8463eccf7f
Robert <nkumrob@gmail.com>
2025-10-27 12:13:52 -0700

    Remove all remaining Supabase dependencies

--- components/reviews/index.ts
+++ components/reviews/index.ts
@@ -7,6 +7,5 @@ export { StarRating } from './star-rating';
 export { ReviewForm } from './review-form';
 export { ReviewCard } from './review-card';
 export { ReviewStatsDisplay } from './review-stats';
-export { TestimonialsSection } from './testimonials-section';
 export { SenjaWidget } from './senja-widget';
 
